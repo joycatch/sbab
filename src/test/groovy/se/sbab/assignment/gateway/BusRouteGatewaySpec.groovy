1b6fad0c9e3c13282e20e0bcd66a85f08f6d7e47
martin@phew.se <martin@phew.se>
2022-07-03 19:23:07 +0200

    Update BusRouteGatewaySpec with additional test

--- src/test/groovy/se/sbab/assignment/gateway/BusRouteGatewaySpec.groovy
+++ src/test/groovy/se/sbab/assignment/gateway/BusRouteGatewaySpec.groovy
@@ -5,25 +5,40 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.client.RestTemplate
 import se.sbab.assignment.domain.JourneyPatternPointOnLine
+import se.sbab.assignment.domain.StopPoint
 import se.sbab.assignment.response.LineDataResponse
 import se.sbab.assignment.response.ResponseData
 import spock.lang.Specification
 
 class BusRouteGatewaySpec  extends Specification {
 
-    def examplePoint1 = JourneyPatternPointOnLine.builder()
+    def journeyPoint1 = JourneyPatternPointOnLine.builder()
             .id(1)
-            .LineNumber(1)
-            .DirectionCode(1)
-            .JourneyPatternPointNumber(1)
+            .lineNumber(1)
+            .directionCode(1)
+            .journeyPatternPointNumber(1)
             .build()
-    def examplePoint2 = JourneyPatternPointOnLine.builder()
+    def journeyPoint2 = JourneyPatternPointOnLine.builder()
             .id(2)
-            .LineNumber(1)
-            .DirectionCode(2)
-            .JourneyPatternPointNumber(2)
+            .lineNumber(1)
+            .directionCode(2)
+            .journeyPatternPointNumber(2)
             .build()
-    def listOfJourneyPatternPointOnLines = [examplePoint1, examplePoint2] as JourneyPatternPointOnLine[]
+    def listOfJourneyPatternPointOnLines = [journeyPoint1, journeyPoint2] as JourneyPatternPointOnLine[]
+
+    def stopPoint1 = StopPoint.builder()
+            .stopPointNumber(1)
+            .stopPointName("A random name")
+            .build()
+    def stopPoint2 = StopPoint.builder()
+            .stopPointNumber(2)
+            .stopPointName("Another name")
+            .build()
+    def stopPoint3 = StopPoint.builder()
+            .stopPointNumber(3)
+            .stopPointName("A third name")
+            .build()
+    def listOfStopPoints = [stopPoint1, stopPoint2, stopPoint3] as StopPoint[]
 
     def objectMapper = new ObjectMapper()
     RestTemplate restTemplate = Mock()
@@ -32,13 +47,13 @@ class BusRouteGatewaySpec  extends Specification {
     def "should return expected amount of JourneyPatternPointOnLine from external source"() {
         given:
         def responseData = ResponseData.builder()
-                .Type("JourneyPatternPointOnLine")
-                .Result(objectMapper.valueToTree(listOfJourneyPatternPointOnLines))
+                .type("JourneyPatternPointOnLine")
+                .result(objectMapper.valueToTree(listOfJourneyPatternPointOnLines))
                 .build()
         def lineDataResponse = LineDataResponse.builder()
-                .StatusCode(200)
-                .ExecutionTime(20)
-                .ResponseData(responseData)
+                .statusCode(200)
+                .executionTime(20)
+                .responseData(responseData)
                 .build()
         restTemplate.getForEntity(_, LineDataResponse.class) >> new ResponseEntity(lineDataResponse, HttpStatus.OK)
 
@@ -47,5 +62,25 @@ class BusRouteGatewaySpec  extends Specification {
 
         then:
         journeyPatternPointOnLine.size() == listOfJourneyPatternPointOnLines.length
+    }
+
+    def "should return expected amount of StopPoints from external source"() {
+        given:
+        def responseData = ResponseData.builder()
+                .type("StopPoint")
+                .result(objectMapper.valueToTree(listOfStopPoints))
+                .build()
+        def lineDataResponse = LineDataResponse.builder()
+                .statusCode(200)
+                .executionTime(20)
+                .responseData(responseData)
+                .build()
+        restTemplate.getForEntity(_, LineDataResponse.class) >> new ResponseEntity(lineDataResponse, HttpStatus.OK)
+
+        when:
+        def stopPoints = busRouteGateway.getStopPoints()
+
+        then:
+        stopPoints.size() == listOfStopPoints.length
     }
 }
